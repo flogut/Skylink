398b81cf47b4aad81b1e4bb740fce12e977c7b79
flogut <florian.gutekunst@gmx.de>
2018-09-06 10:10:10 +0200

    Laden der Daten aus der Datei verbessert - Laden von Dateien mit nur einer Spalte erleichtert (kein Typ mehr nötig) - Laden von Dateien, in denen nicht jeder Datensatz alle Typen enthält ermöglicht

--- src/main/kotlin/de/hgv/controller/DataController.kt
+++ src/main/kotlin/de/hgv/controller/DataController.kt
@@ -17,6 +17,7 @@ class DataController: Controller() {
 
     private var mode: Mode = Mode.DOWNLOAD
     private var file: File? = null
+    private var fileType: FileType? = null
 
     fun getData(type: ContentType): List<Data> = when (mode) {
         Mode.DOWNLOAD -> downloadData(type)
@@ -26,6 +27,7 @@ class DataController: Controller() {
     fun loadFromFile(file: File) {
         mode = Mode.FILE
         this.file = file
+        this.fileType = FileType.of(file)
     }
 
     private fun downloadData(type: ContentType): List<Data> {
@@ -47,34 +49,75 @@ class DataController: Controller() {
     }
 
     private fun getFromFile(type: ContentType): List<Data> {
-        val types = arrayOf(
-            ContentType.TEMPERATURE,
-            ContentType.PRESSURE,
-            ContentType.DUST,
-            ContentType.VOLTAGE,
-            ContentType.LATITUDE,
-            ContentType.LONGITUDE,
-            ContentType.TIME,
-            ContentType.HEIGHT,
-            ContentType.INTERNAL_TEMPERATURE
-        )
+        return when (fileType) {
+            FileType.SINGLE -> getFromFileSingle(type)
+            FileType.MULTIPLE -> getFromFileMultiple(type)
+            null -> emptyList()
+        }
+    }
 
-        val index = types.indexOf(type)
-        val timeIndex = types.indexOf(ContentType.TIME)
+    private fun getFromFileSingle(type: ContentType): List<Data> {
+        val values =
+            file
+                ?.readText()
+                ?.lines()
+                ?.mapNotNull { it.toDoubleOrNull() } ?: return emptyList()
+
+        val millis = System.currentTimeMillis()
+
+        val dataList = mutableListOf<Data>()
+        values.forEachIndexed { i, value ->
+            val id = UUID.randomUUID().toString()
+            val time = Date(millis + 1000 * i)
+
+            dataList.add(Data(id, type, value, time))
+        }
+
+        return dataList
+    }
+
+    private fun getFromFileMultiple(type: ContentType): List<Data> {
+        val types = mapOf(
+            "T" to ContentType.TEMPERATURE,
+            "P" to ContentType.PRESSURE,
+            "D" to ContentType.DUST,
+            "VO" to ContentType.VOLTAGE,
+            "LA" to ContentType.LATITUDE,
+            "LO" to ContentType.LONGITUDE,
+            "TI" to ContentType.TIME,
+            "H" to ContentType.HEIGHT,
+            "TC" to ContentType.INTERNAL_TEMPERATURE
+        )
 
         val lines =
             file
                 ?.readText()
                 ?.lines()
-                ?.filter { it.isNotEmpty() }
-                ?.map { it.removeSuffix("\\") }
-                ?.map { it.split(",").map { it.substringAfter(":") } } ?: return emptyList()
+                ?.filter { it.isNotBlank() }
+                ?.map { it.removeSuffix("\\").removeSuffix("}") }
+                ?.map { line ->
+                    line
+                        .split(",")
+                        .map { it.split(":") }
+                        .filter { it.size == 2 }
+                        .map { types[it[0]] to it[1] }
+                        .filterNot { it.first == null }
+                        .toMap()
+                } ?: return emptyList()
 
         val dataList = mutableListOf<Data>()
         for (line in lines) {
+            val value = line[type]?.toDoubleOrNull() ?: continue
+
+            val date = line[ContentType.TIME] ?: continue
+            val simpleDateFormat = when (date.count { it == ';' }) {
+                2 -> SimpleDateFormat("hh;mm;ss")
+                3 -> SimpleDateFormat("hh;mm;ss;SSS")
+                else -> null
+            } ?: continue
+            val time = simpleDateFormat.parse(line[ContentType.TIME])
+
             val id = UUID.randomUUID().toString()
-            val value = line[index].toDoubleOrNull() ?: continue
-            val time = SimpleDateFormat("hh;mm;ss;SSS").parse(line[timeIndex])
 
             val data = Data(id, type, value, time)
 
@@ -90,5 +133,23 @@ class DataController: Controller() {
 
     private enum class Mode {
         DOWNLOAD, FILE
+    }
+
+    private enum class FileType {
+        SINGLE, MULTIPLE;
+
+        companion object {
+
+            fun of(file: File): FileType? {
+                val text = file.readText()
+                return when {
+                // Eine oder mehr Zeilen mit je einer Dezimalzahl
+                    text.matches(Regex("(\\d+.?\\d*\\r?\\n?)+")) -> SINGLE
+                // Eine oder mehr Zeilen im Format TYP:0.00, TYP2:0.00 [,...]
+                    text.matches(Regex("((\\w+:\\d+.?\\d*,?)+\\r?\\n?)+")) -> MULTIPLE
+                    else -> null
+                }
+            }
+        }
     }
 }
